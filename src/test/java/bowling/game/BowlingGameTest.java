a5695e0639192e92a2615b9849d4c0a01589c2f6
Steve Turley <steve@turleylabs.com>
2020-04-29 15:58:51 -0500

    Testing gutter ball game

--- src/test/java/bowling/game/BowlingGameTest.java
+++ src/test/java/bowling/game/BowlingGameTest.java
@@ -6,9 +6,13 @@ import static org.junit.Assert.*;
 public class BowlingGameTest {
 
     @Test
-    public void something() {
+    public void whenRollingAllGutterBallsThenScoreIsZero() {
         BowlingGame bowlingGame = new BowlingGame();
 
-        assertEquals(false, true);
+        for (int i = 0; i < 20; i++) {
+            bowlingGame.roll(0);
+        }
+
+        assertEquals(0, bowlingGame.score());
     }
 }

5fb52c98329e986c045ab0203496a667762f767a
Steve Turley <steve@turleylabs.com>
2020-04-29 16:11:11 -0500

    Simplest change to make it pass is to sum the pins as they are added and return the result in score.

--- src/main/java/bowling/game/BowlingGame.java
+++ src/main/java/bowling/game/BowlingGame.java
@@ -1,12 +1,13 @@
 package bowling.game;
 
 public class BowlingGame {
+    int score;
 
     public void roll(int pins) {
-
+        score += pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
